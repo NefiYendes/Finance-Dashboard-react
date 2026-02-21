87ff36adaacb2077e77661c996bc0966be477349
Nefi S. Yendes <nefi.s.yendes@gmail.com>
2026-02-21 16:27:12 -0400

    Fue agregada la logica detras de los montos e informacion para mostrar en negativo cuando sea egreso y en positivo ingreso

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import type { ChangeEvent } from "react";
+import type { ChangeEvent, FormEvent } from "react";
 import type { Transaction, Category } from "../types/transaction";
-
-const TransactionForm = () => {
+interface Props {
+  onAddTransaction: (transaction: Transaction) => void;
+};
+const TransactionForm = ({ onAddTransaction }: Props) => {
     const [formData, setFormData] = useState ({
     description: '',
     amount: '',
@@ -17,12 +19,52 @@ const TransactionForm = () => {
             ...prev,
             [name]: value
         }));
+    };
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        
+        const parsedAmount = parseFloat(formData.amount);
+
+        const finalAmount = formData.type.toLowerCase() === 'egreso'
+        ? -Math.abs(parsedAmount)
+        : Math.abs(parsedAmount);
+
+        const newTransaction: Transaction = {
+        id: crypto.randomUUID(),
+        description: formData.description,
+        amount: finalAmount,
+        category: formData.category,
+        date: formData.date,
+    };
+    onAddTransaction(newTransaction);
     }
 
     return (
-        <form style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px' }}>
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px' }}>
+
             <input name="description" placeholder="Description" value={formData.description} onChange={handleChange}/>
+            <input name="amount" type="number" placeholder="Monto" value={formData.amount} onChange={handleChange}/>
+
+            <select name="type" value={formData.type} onChange={handleChange}>
+                <option value="empty">Selecciona una opcion</option>
+                <option value="egreso">Egreso</option>
+                <option value="ingreso">Ingreso</option>
+            </select>
+
+            <select name="category" value={formData.category} onChange={handleChange}>
+                <option value="empty">Selecciona una opcion</option>
+                <option value="comida">Comida</option>
+                <option value="servicios">Servicios</option>
+                <option value="vivienda">Vivienda</option>
+                <option value="entretenimiento">Entretenimiento</option>
+                <option value="transporte">Transporte</option>
+            </select>
+
+            <input name="date" type="date" value={formData.date} onChange={handleChange} />
             <button type="submit">Agregar Transacción</button>
         </form>
     );
 };
+
+export default TransactionForm
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import type { Transaction } from "../types/transaction";
+import TransactionForm from "../components/TransactionForm";
 
 const Dashboard = () => {
-    const [transactions] = useState<Transaction[]> ([
+    const [transactions, setTransactions] = useState<Transaction[]> ([
         {id: '1', description: 'Sueldo', amount: 2500, category: 'Servicios', date: '2026-02-01'},
         {id: '2', description: 'Renta', amount: -800, category: 'Vivienda', date: '2026-02-05'},
         {id: '3', description: 'Cena', amount: -50, category: 'Comida', date: '2026-02-10'}
@@ -10,10 +11,16 @@ const Dashboard = () => {
 
     const expenses = transactions.filter(t => t.amount < 0);
     const totalBalance = transactions.reduce((acc, curr) => acc + curr.amount, 0);
+    const addTransaction = (newTx:Transaction) => {
+        setTransactions(prev => [newTx, ...prev]);
+    };
     return(
         <div style={{padding: '20px', fontFamily: 'Arial, sans-serif'}}>
             <h1>Mi Dashboard Financiero</h1>
-
+        <section style={{ marginBottom: '30px' }}>
+        <h2>Nueva Transacción</h2>
+        <TransactionForm onAddTransaction={addTransaction} />
+      </section>
             <div style={{display: 'flex', gap: '20px', marginBottom: '20px'}}>
 
                 <div style={{padding: '20px', borderRadius: '12px', border:'2px solid #1890ff', backgroundColor: '#e6f7ff'}}>
